42da1d74b570ce2c77d02ef6d1ff671deb499281
bastihz <bastihz.dev@posteo.de>
2021-04-18 21:01:10 +0200

    Show some info

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ A collection of particle emitters.
 
 
 Left click to emit particles.
+F1 to show miscellaneous info.
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
-import pygame
+import pygame.freetype
 
 import src.default
 
@@ -21,6 +21,14 @@ clock = pygame.time.Clock()
 pygame.mouse.set_visible(sim.MOUSE_IS_VISIBLE)
 emitter = sim.Emitter()
 
+show_info = False
+font = pygame.freetype.SysFont("inconsolate, consolas, monospace", 16)
+# invert background color but not the alpha value
+font.fgcolor = pygame.Color([255 - x for x in sim.BACKGROUND_COLOR[:3]])
+print(font.fgcolor)
+line_spacing = pygame.Vector2(0, font.get_sized_height())
+text_margin = pygame.Vector2(5, 5)
+
 while True:
     dt = clock.tick(60) / 1000
 
@@ -30,9 +38,24 @@ while True:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 sys.exit()
+            if event.key == pygame.K_F1:
+                show_info = not show_info
         emitter.handle_event(event)
 
     emitter.update(dt)
 
     emitter.draw(window)
+
+    if show_info:
+        font.render_to(
+            window,
+            text_margin,
+            f"fps: {clock.get_fps():.0f}"
+        )
+        font.render_to(
+            window,
+            text_margin + line_spacing,
+            f"number of particles: {len(emitter.particles)}"
+        )
+
     pygame.display.flip()
