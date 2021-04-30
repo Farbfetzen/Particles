b064db6430e363080a9365d02f9842a0b7575630
bastihz <bastihz.dev@posteo.de>
2021-04-30 12:54:32 +0200

    Implement base.Simulation and base.Emitter

--- src/base.py
+++ src/base.py
@@ -1,59 +1,92 @@
-# import pygame
-#
-#
-# class Simulation:
-#     def __init__(self):
-#         self.particles = []
-#         self.emitters = []
-#         self.is_emitting = False
-#         self.position = pygame.Vector2()
-#         self.previous_position = pygame.Vector2(self.position)
-#         self.velocity = pygame.Vector2()
-#
-#     @property
-#     def n_particles(self):
-#         return len(self.particles)
-#
-#     def handle_event(self, event):
-#         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-#             self.is_emitting = True
-#         elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-#             self.is_emitting = False
-#
-#     def update(self, dt, mouse_position):
-#         self.previous_position.update(self.position)
-#         self.position.update(mouse_position)
-#         self.velocity = (self.position - self.previous_position) / dt * EMITTER_VELOCITY_FACTOR
-#         velocity_change = TOTAL_ACCELERATION * dt
-#         velocity_change_half = velocity_change / 2
-#         alive_particles = []
-#         for p in self.particles:
-#             p.update(dt, velocity_change, velocity_change_half)
-#             if p.alive:
-#                 alive_particles.append(p)
-#         self.particles = alive_particles
-#         n_new_particles = self.emission_timer.update(dt)
-#         if self.is_emitting and n_new_particles > 0:
-#             self.emit(n_new_particles)
-#
-#     def draw(self, target_surface):
-#         pass
-#
-#     def clear(self):
-#         self.emitters.clear()
-#         self.particles.clear()
-#
-#
-# class Emitter:
-#     # Handles particle emission but does not store them. Particles are stored,
-#     # updated and drawn in the Simulation object.
-#     def __init__(self):
-#         pass
-#
-#     def emit(self):
-#         pass
-#
-#
+from abc import ABC, abstractmethod
+
+import pygame
+
+from src.helpers import Timer
+
+
+class Simulation:
+    def __init__(self, background_color, particle_color, particle_radius, total_acceleration):
+        self.particles = []
+        self.emitters = []
+        self.is_emitting = False
+        self.mouse_position = pygame.Vector2()
+        self.previous_mouse_position = pygame.Vector2(self.mouse_position)
+        self.background_color = background_color
+        self.particle_color = particle_color
+        self.particle_radius = particle_radius
+        self.total_acceleration = total_acceleration
+
+    @property
+    def n_particles(self):
+        return len(self.particles)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            self.is_emitting = True
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            self.is_emitting = False
+
+    def update(self, dt, mouse_position):
+        self.previous_mouse_position.update(self.mouse_position)
+        self.mouse_position.update(mouse_position)
+        velocity_change = self.total_acceleration * dt
+        velocity_change_half = velocity_change / 2
+        alive_particles = []
+        for particle in self.particles:
+            particle.update(dt, velocity_change, velocity_change_half)
+            if particle.is_alive:
+                alive_particles.append(particle)
+        self.particles = alive_particles
+        for emitter in self.emitters:
+            emitter.update(dt, self.mouse_position, self.is_emitting, self.particles)
+
+    def draw(self, target_surface):
+        target_surface.fill(self.background_color)
+        if not self.is_emitting:
+            pygame.draw.circle(target_surface, self.particle_color, self.mouse_position, 3, 1)
+        for particle in self.particles:
+            pygame.draw.circle(target_surface, self.particle_color,
+                               particle.position, self.particle_radius)
+
+    def clear(self):
+        self.particles.clear()
+
+
+class Emitter(ABC):
+    def __init__(self, emission_timer_delay, emitter_velocity_factor=0):
+        self.position = pygame.Vector2()
+        self.previous_position = pygame.Vector2()
+        self.emission_timer = Timer(emission_timer_delay)
+        self.emitter_velocity_factor = emitter_velocity_factor
+        self.velocity = pygame.Vector2() if emitter_velocity_factor != 0 else None
+
+    def update(self, dt, mouse_position, is_emitting, particle_list):
+        self.previous_position.update(self.position)
+        self.position.update(mouse_position)
+        if self.velocity is not None:
+            self.velocity.update(
+                (self.position - self.previous_position) / dt * self.emitter_velocity_factor
+            )
+        n_new_particles = self.emission_timer.update(dt)
+        if is_emitting and n_new_particles > 0:
+            self.emit(n_new_particles, particle_list)
+
+    def emit(self, n_particles, particle_list):
+        if self.previous_position.distance_squared_to(self.position) > 0:
+            for i in range(n_particles):
+                position = self.position.lerp(self.previous_position, i / n_particles)
+                self.add_particle(position, particle_list)
+        else:
+            for _ in range(n_particles):
+                self.add_particle(self.position, particle_list)
+
+    @abstractmethod
+    def add_particle(self, position, particle_list):
+        # Instantiate a particle at position and append it to the list.
+        pass
+
+
 # class Particle:
 #     def __init__(self):
 #         pass
--- src/default.py
+++ src/default.py
@@ -2,8 +2,7 @@ import random
 
 import pygame
 
-from src.helpers import Timer
-# from src import base
+from src import base
 
 
 BACKGROUND_COLOR = pygame.Color(0, 0, 32)
@@ -18,87 +17,25 @@ ACCELERATIONS = (
 # I could also implement forces instead of accelerations but that
 # is unnecessary as long as all particles have the same mass.
 TOTAL_ACCELERATION = sum(ACCELERATIONS, pygame.Vector2())
-# Modify the emitter mouse_velocity affecting the initial particle mouse_velocity:
+# Modify the emitter velocity affecting the initial particle velocity:
 EMITTER_VELOCITY_FACTOR = 0.2
 
 
-class Simulation:
+class Simulation(base.Simulation):
     def __init__(self):
-        # super().__init__()
-        self.particles = []
-        self.emitter = Emitter(self, pygame.display.get_window_size())
-        self.is_emitting = False
-        self.mouse_position = pygame.Vector2()
-        self.previous_mouse_position = pygame.Vector2(self.mouse_position)
-        self.mouse_velocity = pygame.Vector2()
-
-    @property
-    def n_particles(self):
-        return len(self.particles)
-
-    def handle_event(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            self.is_emitting = True
-        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-            self.is_emitting = False
-
-    def update(self, dt, mouse_position):
-        self.previous_mouse_position.update(self.mouse_position)
-        self.mouse_position.update(mouse_position)
-        self.mouse_velocity = ((self.mouse_position - self.previous_mouse_position)
-                               / dt * EMITTER_VELOCITY_FACTOR)
-        velocity_change = TOTAL_ACCELERATION * dt
-        velocity_change_half = velocity_change / 2
-        alive_particles = []
-        for p in self.particles:
-            p.update(dt, velocity_change, velocity_change_half)
-            if p.is_alive:
-                alive_particles.append(p)
-        self.particles = alive_particles
-        self.emitter.update(dt, self.mouse_position, self.mouse_velocity, self.is_emitting)
-
-    def draw(self, target_surface):
-        target_surface.fill(BACKGROUND_COLOR)
-        if not self.is_emitting:
-            pygame.draw.circle(target_surface, PARTICLE_COLOR, self.mouse_position, 3, 1)
-        for p in self.particles:
-            pygame.draw.circle(target_surface, PARTICLE_COLOR, p.position, PARTICLE_RADIUS)
-
-    def clear(self):
-        self.particles.clear()
+        super().__init__(BACKGROUND_COLOR, PARTICLE_COLOR, PARTICLE_RADIUS, TOTAL_ACCELERATION)
+        self.emitters.append(Emitter(pygame.display.get_window_size()))
 
 
-class Emitter:
-    def __init__(self, simulation, window_size):
-        self.simulation = simulation
-        self.position = pygame.Vector2()
-        self.previous_position = pygame.Vector2()
-        self.emission_timer = Timer(1 / PARTICLES_PER_SECOND)
+class Emitter(base.Emitter):
+    def __init__(self, window_size):
+        super().__init__(1 / PARTICLES_PER_SECOND, EMITTER_VELOCITY_FACTOR)
         self.window_right, self.window_bottom = window_size
 
-    def update(self, dt, position, velocity, is_emitting):
-        self.previous_position.update(self.position)
-        self.position.update(position)
-        n_new_particles = self.emission_timer.update(dt)
-        if is_emitting and n_new_particles > 0:
-            self.emit(n_new_particles, velocity)
-
-    def emit(self, n_particles, velocity):
-        if self.previous_position.distance_squared_to(self.position) > 0:
-            for i in range(n_particles):
-                position = self.position.lerp(self.previous_position, i / n_particles)
-                self.simulation.particles.append(
-                    Particle(position, self.window_right, self.window_bottom, velocity)
-                )
-            # TODO: Can be optimized. If distance is 10 px and n_particles is 30
-            #  then I need only 10 interpolation steps with 3 particles each.
-            #  Currently it interpolates 30 times which is unnecessary.
-            #  Attention: In that case use distance_to instead of distance_squared_to.
-        else:
-            for _ in range(n_particles):
-                self.simulation.particles.append(
-                    Particle(self.position, self.window_right, self.window_bottom, velocity)
-                )
+    def add_particle(self, position, particle_list):
+        particle_list.append(
+            Particle(position, self.window_right, self.window_bottom, self.velocity)
+        )
 
 
 class Particle:
@@ -121,6 +58,4 @@ class Particle:
             self.position += (self.velocity - velocity_change_half) * dt
             # This one is inaccurate but also ok. The difference gets smaller
             # with time and becomes less than 1 % after about 120 steps.
-            # self.mouse_position += self.mouse_velocity * dt
-
-
+            # self.mouse_position += self.velocity * dt
