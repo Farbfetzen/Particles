5de75007f2f13ab3c4053682b819134ea21be96c
bastihz <bastihz.dev@posteo.de>
2021-05-01 21:35:08 +0200

    Bounce against all walls and remove gravity

--- src/bounce.py
+++ src/bounce.py
@@ -7,23 +7,19 @@ from src.helpers import linear_map
 
 
 PARTICLE_COLOR = pygame.Color(64, 196, 64)
-PARTICLE_DIAMETER = 10
+PARTICLE_DIAMETER = 20
 PARTICLE_RADIUS = PARTICLE_DIAMETER / 2
 PARTICLES_PER_SECOND = 100
 EMISSION_DELAY = 1 / PARTICLES_PER_SECOND
-SPEED_MEAN = 50  # pixels per second
-SPEED_SD = 10
-ACCELERATIONS = (
-    pygame.Vector2(0, 750),  # gravity
-)
-# I could also implement forces instead of accelerations but that
-# is unnecessary as long as all particles have the same mass.
-TOTAL_ACCELERATION = sum(ACCELERATIONS, pygame.Vector2())
+SPEED_MEAN = 200
+SPEED_SD = 25
+TOTAL_ACCELERATION = 0
 EMITTER_VELOCITY_FACTOR = 0.2
 BOUNCE_MODIFIER_ALPHABETA = 3
-BOUNCE_MODIFIER_MIN = 0.7
-BOUNCE_MODIFIER_MAX = 0.8
-MAX_BOUNCES = 10
+BOUNCE_MODIFIER_MIN = 0.8
+BOUNCE_MODIFIER_MAX = 0.9
+LIFETIME_MEAN = 10
+LIFETIME_SD = 2
 
 
 class BounceSimulation(Simulation):
@@ -50,7 +46,8 @@ def make_particle_image():
 
 class BounceParticle(Particle):
     image = make_particle_image()
-    x_min = -PARTICLE_DIAMETER
+    x_min = 0
+    y_min = 0
     x_max = 1200
     y_max = 800 - PARTICLE_DIAMETER
 
@@ -59,7 +56,7 @@ class BounceParticle(Particle):
         # Adjust particle position limits in case the window size
         # was changed via the command line argument:
         width, height = pygame.display.get_window_size()
-        cls.x_max = width
+        cls.x_max = width - PARTICLE_DIAMETER
         cls.y_max = height - PARTICLE_DIAMETER
 
     def __init__(self, position, emitter_velocity):
@@ -68,21 +65,29 @@ class BounceParticle(Particle):
         self.velocity = pygame.Vector2(random.gauss(SPEED_MEAN, SPEED_SD), 0)
         self.velocity.rotate_ip(random.uniform(0, 360))
         self.velocity += emitter_velocity
-        self.bounces = 0
         self.bounce_velocity_modifier = -linear_map(
             random.betavariate(BOUNCE_MODIFIER_ALPHABETA, BOUNCE_MODIFIER_ALPHABETA),
             out_start=BOUNCE_MODIFIER_MIN,
             out_end=BOUNCE_MODIFIER_MAX
         )
+        self.time = 0
+        self.lifetime_limit = random.gauss(LIFETIME_MEAN, LIFETIME_SD)
 
     def update(self, dt, velocity_change, velocity_change_half):
-        self.velocity += velocity_change
-        self.position += (self.velocity - velocity_change_half) * dt
-        if self.position.y >= BounceParticle.y_max:
+        self.time += dt
+        if self.time >= self.lifetime_limit:
+            self.is_alive = False
+            return
+        self.position += self.velocity * dt
+        if self.position.x <= BounceParticle.x_min:
+            self.position.x = -self.position.x
+            self.velocity.x *= self.bounce_velocity_modifier
+        elif self.position.x >= BounceParticle.x_max:
+            self.position.x = BounceParticle.x_max * 2 - self.position.x
+            self.velocity.x *= self.bounce_velocity_modifier
+        if self.position.y <= BounceParticle.y_min:
+            self.position.y = -self.position.y
+            self.velocity.y *= self.bounce_velocity_modifier
+        elif self.position.y >= BounceParticle.y_max:
             self.position.y = BounceParticle.y_max * 2 - self.position.y
             self.velocity.y *= self.bounce_velocity_modifier
-            self.bounces += 1
-            if self.bounces >= MAX_BOUNCES:
-                self.is_alive = False
-        elif not BounceParticle.x_min < self.position.x < BounceParticle.x_max:
-            self.is_alive = False
